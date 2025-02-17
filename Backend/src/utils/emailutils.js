161d6a5f89bef4567e7c26b6d455af11eb1c144e
Ayuussshhh <149790144+Ayuussshhh@users.noreply.github.com>
2025-02-17 13:33:52 +0530

    Add files via upload

--- Backend/src/utils/emailutils.js
+++ Backend/src/utils/emailutils.js
@@ -1,1 +1,31 @@
+// utils/emailUtils.js
+import nodemailer from 'nodemailer';
 
+export const sendEmail = async (to, subject, body, attachmentPath) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text: body,
+      attachments: [
+        {
+          path: attachmentPath, // Path to the PDF ticket
+        },
+      ],
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
